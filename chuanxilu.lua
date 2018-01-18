ec1f5a54c0b97252ab1550e6cc65e0cd7a516c1c
songzhentao <zt.song@holaverse.com>
2018-01-18 19:29:00 +0800

    newbook

---
+++ chuanxilu.lua
@@ -0,0 +1,24 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: songzhentao
+-- Date: 2018/1/14
+-- Time: 上午10:37
+-- To change this template use File | Settings | File Templates.
+--
+
+require 'import'
+import 'org.jsoup.nodes.Document'
+import 'org.jsoup.nodes.Element'
+import 'org.jsoup.select.Elements'
+--
+--require 'kankanba_base'
+require 'https://raw.githubusercontent.com/muchenshou/booklibrary/master/kankanba_base.lua'
+
+
+function fetchChapterList()
+    return KanKanBa.fetchChapterList("http://www.sbkk88.com/mingzhu/gudaicn/chuanxilu/")
+end
+
+function fetchChapterContent(url)
+    return KanKanBa.fetchChapterContent(url)
+end
