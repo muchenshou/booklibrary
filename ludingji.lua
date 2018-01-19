2ae5fbda3c3217fe2fb6fa9f92871230bf59b758
songzhentao <zt.song@holaverse.com>
2018-01-19 18:27:47 +0800

    jinyong

---
+++ ludingji.lua
@@ -0,0 +1,24 @@
+
+--
+-- Created by IntelliJ IDEA.
+-- User: songzhentao
+-- Date: 2018/1/14
+-- Time: 上午10:37
+-- To change this template use File | Settings | File Templates.
+--
+
+require 'import'
+import 'org.jsoup.nodes.Document'
+import 'org.jsoup.nodes.Element'
+import 'org.jsoup.select.Elements'
+
+require 'https://raw.githubusercontent.com/muchenshou/booklibrary/master/script/jinyongwang.lua'
+
+function fetchChapterList()
+    return Jinyongwang.fetchChapterList("http://www.jinyongwang.com/lu/")
+end
+
+function fetchChapterContent(url)
+    return Jinyongwang.fetchChapterContent(url)
+end
+
